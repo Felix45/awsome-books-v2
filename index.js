d5d094c2abe1081ef2986eb9461265dfec6c0666
Felix Ouma <fatonoh@gmail.com>
2022-05-30 18:16:50 +0300

    Import ES6 modules to use in homepage

---
+++ index.js
@@ -0,0 +1,17 @@
+import BookStore from './modules/BookStore.js';
+import {
+  form, menuItems, showSection, displayDate,
+} from './modules/EventHandlers.js';
+
+const bookstore = new BookStore();
+
+menuItems.forEach((menuItem) => menuItem.addEventListener('click', showSection));
+
+window.onload = () => { bookstore.displayBooks(); };
+setInterval(displayDate, 1000);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  bookstore.addBook(form.elements.title.value, form.elements.author.value);
+  form.reset();
+});
